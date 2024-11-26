39052aa3bb930838773fc27894ee6f271e96c095
Hamzarfs <hamzasarwar.rfs@gmail.com>
2024-11-27 00:24:54 +0500

    adsas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/common/header';
+import '../App.css'
 
 const Home = () => {
     return (
+        
         <div>
+            <Header/>
             <h1>Home Page</h1>
             <p>Welcome to our website!</p>
             <Link to="/about">Go to About Us</Link>
