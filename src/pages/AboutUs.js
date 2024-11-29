dad0710366d42d6e09543c323f961cec7ee3ee42
Hamzarfs <hamzasarwar.rfs@gmail.com>
2024-11-29 17:13:57 +0500

    mergre dev into hamza

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/common/header';
+import Footer from '../components/common/footer';
+import ContactUsform from '../components/Contactus/contactus';
 
 const About = () => {
     return (
-        <div className='about-us-page'>
-            <Header />
-            <section className='about-banner'>
-                
-            </section>
+        <div>
+            <Header/>
+            <ContactUsform/>
+            <Footer/>
         </div>
     );
 };
